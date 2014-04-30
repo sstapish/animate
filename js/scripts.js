ee21a772b2c109b7c222ae94632df50d97041ce2
Stapish <sgraham74@engr-134-06.eastern.ewu.edu>
2014-04-30 14:39:43 -0700

    transit

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,22 @@
 // My Scripts
 $(document).foundation();
+
+//animate.css stuff
+
+$('h1').click(function(){
+
+$('img').addClass('animated rubberBand');
+
+//console.log('click me');
+
+});
+
+// Transit
+$('img').click(function(){
+
+	$('p').transition({ 
+		x: 200,
+		y: 200
+
+	});
+});
